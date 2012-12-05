33cc1b8443e6abe6947c092a65b68b95012b4691
Shmuel Fomberg <shmuelfomberg@gmail.com>
2012-12-05 15:47:56 +0900

    falling back to SDBM_File if DB_File is not installed

--- lib/Dist/Surveyor/DB_File.pm
+++ lib/Dist/Surveyor/DB_File.pm
@@ -1,6 +1,19 @@
 package Dist::Surveyor::DB_File;
-use base 'DB_File';
+use strict;
+use warnings;
 use Storable qw(freeze thaw);
+
+our @ISA;
+if    (eval { require DB_File;   1; }) {
+    @ISA = ('DB_File');
+
+}
+elsif (eval { require SDBM_File; 1; }) {
+    @ISA = ('SDBM_File');
+}
+else {
+    die "Need either DB_file or SDBM_File installed to run";
+}
 
 # DB_File can store only strings as values, and not Perl structures
 # this small wrapper fixes the problem
