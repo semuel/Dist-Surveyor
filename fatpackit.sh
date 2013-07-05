d49068f46c97fe631b52b6164de18b682e261366
Shmuel Fomberg <shmuelfomberg@gmail.com>
2013-07-05 11:03:00 +0900

    fixing the release process

--- fatpackit.sh
+++ fatpackit.sh
@@ -5,4 +5,4 @@ fatpack packlists-for `cat fatpacker.trace` > fatpacker.packlists
 ./process_fatpacker_packlist.pl fatpacker.packlists
 fatpack tree `cat fatpacker.packlists`
 (fatpack file; cat bin/dist_surveyor ) > dist_surveyor_packed.pl
-rm fatpacker.trace fatpacker.packlists
+rm -R fatpacker.trace fatpacker.packlists fatlib
